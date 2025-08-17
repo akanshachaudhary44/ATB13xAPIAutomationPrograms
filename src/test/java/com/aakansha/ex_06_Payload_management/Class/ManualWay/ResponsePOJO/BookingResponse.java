5d9a40968db93129870d7b28329390b1d9897957
akanshachaudhary44 <akanshachaudhary44@gmail.com>
2025-08-17 09:29:59 +0530

    API Rest assured testing

---
+++ src/test/java/com/aakansha/ex_06_Payload_management/Class/ManualWay/ResponsePOJO/BookingResponse.java
@@ -0,0 +1,24 @@
+package com.aakansha.ex_06_Payload_management.Class.ManualWay.ResponsePOJO;
+
+import com.aakansha.ex_06_Payload_management.Class.ManualWay.RequestPOJO.Booking;
+
+public class BookingResponse {
+    private Integer bookingid;
+    private Booking booking;
+
+    public Integer getBookingid() {
+        return bookingid;
+    }
+
+    public void setBookingid(Integer bookingid) {
+        this.bookingid = bookingid;
+    }
+
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
+}
